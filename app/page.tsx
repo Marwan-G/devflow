ffbaac0ca5d4ff570fe8c9458e6dc7a6c7972061
Marwan-G <ghabin2004@gmail.com>
2024-11-10 22:20:43 +0000

    setup nodejx app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Home = () => (
+    <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Nextjs World
+    </h1>
+);
+
+export default Home
