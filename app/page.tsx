e33fa37752211bbc3241c17c0aa4dba35e0061ec
Marwan-G <ghabin2004@gmail.com>
2024-11-11 20:04:14 +0000

    Eslint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
 const Home = () => (
-    <h1 className="text-3xl text-violet-700 font-black">
-        Welcome to Nextjs World
-    </h1>
+    <h1 className='text-3xl text-violet-700 font-black'>Welcome to Nextjs</h1>
 );
 
 export default Home
