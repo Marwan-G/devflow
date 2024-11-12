13dedf6a3cf81c1879be2c5396ebed43a8d2dcd1
Marwan-G <ghabin2004@gmail.com>
2024-11-12 23:16:20 +0000

    TailwindCSS setup Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => (
-    <h1 className='text-3xl text-violet-700 font-black'>Welcome to Nextjs</h1>
+  <h1 className="text-3xl text-violet-700 font-black">
+    Welcome to DevFlow app
+  </h1>
 );
 
-export default Home
+export default Home;
