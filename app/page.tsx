28a4d51990eacf201ae8ddb764d4d86fa7d01096
Marwan-G <ghabin2004@gmail.com>
2024-11-17 16:00:10 +0000

    initial navbar ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => (
   <>
-    <h1 className="text-3xl text-violet-700 font-black dark:">
-      Welcome to DevFlow app
-    </h1>
     <h1 className="text-3xl text-violet-700 text-tahiti-dark  font-space-grotesk">
       Welcome to DevFlow app
     </h1>
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </>
 );
 
